fc15b1162c20fde7f1eacbfe909f7794f8853ed0
Heurtault Aubin <aub.heurtault@gmail.com>
2022-09-18 17:16:23 +0200

    setup tailwind css 3

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex justify-center items-center h-screen">
+    <h1 class="text-3xl font-bold text-green-500">
+      Tailwind CSS 3 is installed in Nuxt 3
+    </h1>
   </div>
 </template>
